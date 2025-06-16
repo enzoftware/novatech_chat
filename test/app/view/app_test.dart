d98914eb54888b4e4144592f4b67131c5fd1979d
Enzo Lizama <lizama.enzo@gmail.com>
2025-06-16 15:08:24 -0500

    feat: Enhance app tests with mock implementations for FirebaseAuth and GoogleSignIn

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,11 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:novatech_chat/app/app.dart';
 import 'package:novatech_chat/counter/counter.dart';
+
+class MockFirebaseAuth extends Mock implements FirebaseAuth {}
+
+class MockGoogleSignIn extends Mock implements GoogleSignIn {}
 
 void main() {
   group('App', () {
     testWidgets('renders CounterPage', (tester) async {
-      await tester.pumpWidget(const App());
+      await tester.pumpWidget(
+        NolatechApp(
+          firebaseAuth: MockFirebaseAuth(),
+          googleSignIn: MockGoogleSignIn(),
+        ),
+      );
       expect(find.byType(CounterPage), findsOneWidget);
     });
   });
